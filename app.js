5402c1a59d900422a09eb1c9eab8485003bc23b9
tetsu123 <perfect_blue@windowslive.com>
2021-06-07 22:23:16 +0700

    cron

--- app.js
+++ app.js
@@ -2,13 +2,38 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 const port = process.env.PORT || 8080;
+const cron = require('node-cron');
+const unirest = require('unirest');
 
 app.use(bodyParser.json({limit: '50mb'}));
 
-// app.get('/', (req, res) => {
-//     console.log('first');
-//     res.send('Hello');
-// })
+cron.schedule(5000, async () => {
+    try {
+        let channelToken = 'TJ7zvFrDTb+4rd0LLqeDh5QVk8IqbNeLvXbHG5Prrxwd5fhO+RuDpUjgeisY50Tsb026CZQz1+ZMBYUIZV5Q7JCz/58rrKoyoLBXhsscU4fksVi+SFfTpYRBVhH5GDtvxGjQKp5vAemKZPMBMN7WhAdB04t89/1O/w1cDnyilFU=';
+        let linePushAPI = 'https://api.line.me/v2/bot/message/push';
+        let headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + channelToken
+        };
+        let messages = {
+            "to": "C9f1ecd6ee355ebd259d32fd3a742208d",
+            "messages": [{
+                "type": "text",
+                "text": "ถามใคร ใครก็รู้"
+            }]
+        };
+        unirest.post(linePushAPI)
+        .headers(headers)
+        .timeout(30)
+        .send(messages)
+        .end((response) => {
+            console.log('response', response);
+        })
+    } catch (error) {
+
+    }
+})
+
 app.use('/webhook', require('./controllers/line-notify/index'));
 
 // const https = require('https');
