f376c5e55d8b3cdf410dba10ec65480350b353ac
tetsu123 <perfect_blue@windowslive.com>
2021-06-07 19:44:58 +0700

    asd

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json({limit: '50mb'}));
 
 app.use('/line', require('./controllers/line-notify/index'));
 
-const https = require('https');
-https.createServer(app).listen(4200, function() {
-    console.log(`> Ready on https://localhost:4201`);
+// const https = require('https');
+// https.createServer(app).listen(4200, function() {
+//     console.log(`> Ready on https://localhost:4201`);
+// });
+
+app.listen(port, () => {
+    console.log(`> Ready on https://localhost: ${port}`);
 });
   
 module.exports = app;
